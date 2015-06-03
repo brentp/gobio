104ca7a45ed18eb4a71b986f4e4fd0f3c384cbe0
Brent Pedersen (brentp) <bpederse@gmail.com>
2015-06-03 10:32:03 -0600

    add mult-sample somatic variant caller

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ strip-sam-aux
 
 remove select aux tags from a *bam* file (reads a bam, writes a stripped bam)
 
+somatics
+--------
+
+call somatic variants on a multi-sample VCF with a single normal sample and many tumor samples (from the same patient, presumably across time).
+A site where any tumor sample has a variant relative to the normal will have PASS or "." as the filter. This uses
+the genotype-likelihood method from @chapmanb 's bcbio and @cc2qe 's speedseq, but handles multiple tumor samples.
+
 
---
+++ somatics/somatics.go
@@ -0,0 +1,160 @@
+// call somatic variants among multiple normal samples. This uses the genotype likelihood
+// approach from bcbio and speedseq but allows for multiple tumor samples. Anything that
+// meets the criteria will have a "PASS" or "." FILTER and a list of tumor samples with the
+// somatic variant in INFO["SOMATIC"]
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/brentp/vcfgo"
+	"github.com/brentp/xopen"
+)
+
+func getTumorLOD(gls []float64) float64 {
+	lod := -1.0
+	if len(gls) < 3 {
+		return lod
+	}
+	for _, gl := range gls[1:] {
+		t := gl - gls[0]
+		if t > lod {
+			lod = t
+		}
+	}
+	return lod
+}
+
+func getNormalLOD(gls []float64, thresh float64) float64 {
+	lod := 1e7
+	if len(gls) < 3 {
+		return thresh
+	}
+	for _, gl := range gls[1:] {
+		t := gls[0] - gl
+		if t < lod {
+			lod = t
+		}
+	}
+	return lod
+}
+
+func somaticLOD(normalGLs []float64, tumorGLs []float64, thresh float64) bool {
+	tumorLOD := getTumorLOD(tumorGLs)
+	normalLOD := getNormalLOD(normalGLs, thresh)
+	return tumorLOD >= thresh && normalLOD >= thresh
+}
+
+func getFreq(refCount int, altCounts []int) float64 {
+	ac := 0
+	for _, a := range altCounts {
+		ac += a
+	}
+	return float64(ac) / float64(refCount+ac)
+}
+
+var THRESH_RATIO = 2.7
+
+func somaticFreqs(normal *vcfgo.SampleGenotype, tumor *vcfgo.SampleGenotype) bool {
+	var nFreq, tFreq float64
+	nrd, err := normal.RefDepth()
+	if err != nil {
+		nFreq = 0.0
+	} else {
+		nads, err := normal.AltDepths()
+		if err != nil {
+			nFreq = 0.0
+		} else {
+			nFreq = getFreq(nrd, nads)
+		}
+	}
+	trd, err := tumor.RefDepth()
+	if err != nil {
+		tFreq = 0.0
+	} else {
+		tads, err := tumor.AltDepths()
+		if err != nil {
+			tFreq = 0.0
+		} else {
+			tFreq = getFreq(trd, tads)
+		}
+	}
+	return nFreq <= 0.001 || nFreq <= tFreq/THRESH_RATIO
+
+}
+
+func Somatics(v *vcfgo.Variant, normalIdx int) []string {
+	thresh := 3.5
+
+	normal := v.Samples[normalIdx]
+	somatics := make([]string, 0)
+	for i, tumor := range v.Samples {
+		if i == normalIdx {
+			continue
+		}
+		if !somaticFreqs(normal, tumor) {
+			continue
+		}
+		if somaticLOD(normal.GL, tumor.GL, thresh) {
+			somatics = append(somatics, v.Header.SampleNames[i])
+		}
+	}
+	return somatics
+}
+
+func check(e error) {
+	if e != nil {
+		log.Fatal(e)
+	}
+}
+
+func main() {
+	index := flag.Int("index", 0, "0-based index of the normal sample")
+	flag.Parse()
+	vcfs := flag.Args()
+	if len(vcfs) != 1 {
+		fmt.Printf("----------------- call somatic variants present in any tumor sample --------------\n")
+		fmt.Printf("----- uses the method from bcbio and speedseq but for multiple tumor samples ------")
+		fmt.Printf("%s -index 0 normal-and-tumors.vcf.gz", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	fhr, err := xopen.Ropen(vcfs[0])
+	check(err)
+
+	rdr, err := vcfgo.NewReader(fhr, false)
+	check(err)
+
+	fhw, err := xopen.Wopen("-")
+	check(err)
+
+	hdr := rdr.Header
+	hdr.Filters["NOT_SOMATIC"] = "not somatic between normal and any tumor"
+
+	hdr.Infos["SOMATIC"] = &vcfgo.Info{Id: "SOMATIC", Number: "1", Type: "String", Description: "Tumor samples with somatic event"}
+
+	wtr, err := vcfgo.NewWriter(fhw, hdr)
+	check(err)
+
+	for v := rdr.Read(); v != nil; v = rdr.Read() {
+
+		somatics := Somatics(v, *index)
+		if len(somatics) > 0 {
+			v.Info.Add("SOMATIC", strings.Join(somatics, "|"))
+		} else {
+			if v.Filter == "." || v.Filter == "PASS" {
+				v.Filter = ""
+			}
+			if v.Filter != "" {
+				v.Filter += ";"
+			}
+			v.Filter += "NOT_SOMATIC"
+		}
+		wtr.WriteVariant(v)
+	}
+}
